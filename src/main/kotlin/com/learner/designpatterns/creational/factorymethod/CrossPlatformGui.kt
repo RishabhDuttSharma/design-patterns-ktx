d49ec6f585c68a1cba06a375cbd13c82a76a3e14
Rishabh Dutt Sharma <rishabhduttsharma@gmail.com>
2020-05-30 11:33:23 +0530

    added tech doc

--- src/main/kotlin/com/learner/designpatterns/creational/factorymethod/CrossPlatformGui.kt
+++ src/main/kotlin/com/learner/designpatterns/creational/factorymethod/CrossPlatformGui.kt
@@ -17,85 +17,124 @@
 package com.learner.designpatterns.creational.factorymethod
 
 /**
+ * Cross Platform GUI
+ * -------------------
+ *
+ * A cross-platform gui library may have support to several platforms.
+ * For each GUI component, it would have a specific implementation for
+ * each of the platforms.
+ *
+ * A Button, for instance, would have two different implementations each
+ * for Windows and MacOS, assuming the library supports both of them.
+ * To maintain the contract, an abstract layer for Button would be required
+ * for the concrete implementations.
+ *
+ * Further, for a Dialog that consists of a "ACTION" Button, would also have
+ * two different implementations. Again, to maintain the contract, the
+ * Dialog would have also have a layer of abstraction. Since the Dialog
+ * cannot predict which implementation of Button would be required, thus
+ * it would leave the task of creating Button to sub-classes. Thus, the
+ * Dialog abstraction-layer would have an abstract-method for the creation
+ * of Button, which is left for the sub-classes to implement. This abstract
+ * method is called FACTORY-METHOD.
+ *
+ * As a result, WindowsDialog will create a WindowsButton and MacOsDialog will
+ * create a MacOsButton, using their factory-method.
+ *
  *
  * Created by Rishabh on 29-05-2020
  */
 
-// Product
+
+/**
+ * Abstraction layer for the Button
+ * Defines the contract for Button implementations
+ */
 interface IButton {
 
+    /**
+     * Draws the Button
+     */
     fun draw()
-
-    fun onClick()
 }
 
 
-// Concrete Product - MacOS
+/**
+ * Concrete implementation of Button for MacOS
+ */
 class MacOsButton : IButton {
 
-    override fun draw() {
-        println("MacOS -> Drawing Button using XCode Tools...")
-    }
-
-    override fun onClick() {
-        println("Opening Finder...")
-    }
+    override fun draw() = println("MacOS -> Drawing Button using XCode Tools...")
 }
 
-// Concrete Product - Windows
+/**
+ * Concrete implementation of Button for Windows
+ */
 class WindowsButton : IButton {
 
-    override fun draw() {
-        println("Windows -> Drawing Button using VisualStudio Tools...")
-    }
-
-    override fun onClick() {
-        println("Opening Explorer...")
-    }
+    override fun draw() = println("Windows -> Drawing Button using VisualStudio Tools...")
 }
 
-// Factory
+/**
+ * Abstraction layer for a Dialog that consists of a Button
+ * Defines the contract for the Dialog implementations
+ */
 abstract class IDialog {
 
-    private lateinit var okButton: IButton
-
+    /**
+     * Renders the Dialog, and its components
+     */
     fun render() {
         // create Button using factory-method
-        okButton = createButton()
-        // draw the button on screen
-        okButton.draw()
+        createButton().also { actionButton ->
+            // draw the button on screen
+            actionButton.draw()
+        }
     }
 
-    fun performActionEvent() {
-        // invoke the on-click event for Button
-        okButton.onClick()
-    }
-
-    // Factory-method
-    abstract fun createButton(): IButton
+    /**
+     * Factory-Method for creating the Button
+     *
+     * NOTE: This method must be implemented by sub-classes to return
+     * an appropriate instance of [IButton]
+     *
+     * @return instance of [IButton]
+     */
+    protected abstract fun createButton(): IButton
 }
 
+/**
+ * Concrete implementation of Dialog for Windows
+ */
 class WindowsDialog : IDialog() {
+
+    /** Factory-method: Creates WindowsButton */
     override fun createButton(): IButton = WindowsButton()
 }
 
+
+/**
+ * Concrete implementation of Dialog for MacOs
+ */
 class MacOsDialog : IDialog() {
+
+    /** Factory-method: Creates MacOsButton */
     override fun createButton(): IButton = MacOsButton()
 }
 
+/** playground */
 fun main() {
+    // evaluate the target-platform
+    when (Platform.values().random()) {
+        // create dialog based on the platform
+        Platform.WINDOWS -> WindowsDialog()
+        Platform.MAC_OS -> MacOsDialog()
+    }.render()  // show dialog
+}
 
-    WindowsDialog().run {
-        // show dialog
-        render()
-        // perform click on the ok-button
-        performActionEvent()
-    }
-
-    MacOsDialog().run {
-        // show dialog
-        render()
-        // perform click on the ok-button
-        performActionEvent()
-    }
+/**
+ * Represents the Platforms supported by GUI-Library
+ */
+enum class Platform {
+    WINDOWS, MAC_OS
 }
