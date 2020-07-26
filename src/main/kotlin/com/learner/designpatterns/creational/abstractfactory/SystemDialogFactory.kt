5563bba62612e2de08a669057d4a97aca18abe96
Rishabh Dutt Sharma <rishabhduttsharma@gmail.com>
2020-07-26 19:06:56 +0530

    added tech doc to methods

--- src/main/kotlin/com/learner/designpatterns/creational/abstractfactory/SystemDialogFactory.kt
+++ src/main/kotlin/com/learner/designpatterns/creational/abstractfactory/SystemDialogFactory.kt
@@ -17,77 +17,152 @@
 package com.learner.designpatterns.creational.abstractfactory
 
 /**
- *
+ * System-Dialog-Factory
+ * ---------------------
  * Created by Rishabh on 26-07-2020
  */
 
-enum class SystemConfig(val systemType: String) {
+/**
+ * Enumeration that wraps dialog-configurations for different systems.
+ */
+enum class DialogConfig(val systemType: String) {
     Windows("Windows-Dialog"),
     MacOs("MacOs-Dialog")
 }
 
-abstract class SystemDialog(private val systemConfig: SystemConfig) {
+/**
+ * Represents the abstraction-layer for implementing a dialog for an underlying-system.
+ * {abstract-product}
+ *
+ * @param dialogConfig the configuration of the underlying-system
+ */
+abstract class SystemDialog(private val dialogConfig: DialogConfig) {
 
-    abstract fun showContents()
+    /**
+     * Shows the contents specific to a System-Dialog.
+     * It should be overridden by child-classes to show their contents.
+     */
+    protected abstract fun showContents()
 
+    /**
+     * Displays dialog on the Screen according to the provided [DialogConfig].
+     * It internally calls the [showContents] to show implementation-specific contents.
+     */
     fun show() {
-        println("======= ${systemConfig.systemType} =======")
+        println("======= ${dialogConfig.systemType} =======")
         showContents()
         println("======= END =======")
     }
 }
 
+/**
+ * [SystemDialog] that shows an information-message on the screen
+ * {concrete-product: information-dialog}
+ *
+ * @param dialogConfig [DialogConfig] for the underlying-system
+ * @param message the information to be displayed
+ */
 class SimpleDialog(
-    systemConfig: SystemConfig,
+    dialogConfig: DialogConfig,
     private val message: String
-) : SystemDialog(systemConfig) {
+) : SystemDialog(dialogConfig) {
 
+    /**
+     * Shows the [message] on screen
+     */
     override fun showContents() {
         println("Message: $message")
     }
 }
 
+/**
+ * [SystemDialog] that shows an information-message along with an action on the screen
+ * {concrete-product: action-dialog}
+ *
+ * @param dialogConfig [DialogConfig] for the underlying-system
+ * @param message the information to be displayed
+ * @param action the action that can be performed on this dialog
+ *
+ */
 class ActionDialog(
-    systemConfig: SystemConfig,
+    dialogConfig: DialogConfig,
     private val message: String,
     private val action: String
-) : SystemDialog(systemConfig) {
+) : SystemDialog(dialogConfig) {
 
+    /**
+     * Show [message] and [action] on the screen
+     */
     override fun showContents() {
         println("Message: $message")
         println("Action: $action")
     }
 }
 
+/**
+ * Abstraction-layer for implementing a Factory for creating [SystemDialog]s for
+ * different underlying-systems.
+ *
+ * {abstract-factory: system-dialog-factory}
+ */
 interface SystemDialogFactory {
 
+    /**
+     * Creates instance of [SimpleDialog] for a given system-implementation
+     */
     fun createDialog(message: String): SimpleDialog
 
+    /**
+     * Creates instance of [ActionDialog] for a given system-implementation
+     */
     fun createActionDialog(message: String, action: String): ActionDialog
 }
 
+/**
+ * [SystemDialogFactory] implementation for creating [SystemDialog]s for Windows
+ *
+ * {concrete-factory: windows-dialog-factory}
+ */
 class WindowsDialogFactory : SystemDialogFactory {
 
+    /**
+     * Creates instance of [SimpleDialog] with [DialogConfig.Windows] configuration
+     */
     override fun createDialog(message: String): SimpleDialog {
-        return SimpleDialog(SystemConfig.Windows, message)
+        return SimpleDialog(DialogConfig.Windows, message)
     }
 
+    /**
+     * Creates instance of [ActionDialog] with [DialogConfig.Windows] configuration
+     */
     override fun createActionDialog(message: String, action: String): ActionDialog {
-        return ActionDialog(SystemConfig.Windows, message, action)
+        return ActionDialog(DialogConfig.Windows, message, action)
     }
 }
 
+/**
+ * [SystemDialogFactory] implementation for creating [SystemDialog]s for MacOs
+ *
+ * {concrete-factory: mac-os-dialog-factory}
+ */
 class MacOsDialogFactory : SystemDialogFactory {
 
+    /**
+     * Creates instance of [SimpleDialog] with [DialogConfig.MacOs] configuration
+     */
     override fun createDialog(message: String): SimpleDialog {
-        return SimpleDialog(SystemConfig.MacOs, message)
+        return SimpleDialog(DialogConfig.MacOs, message)
     }
 
+    /**
+     * Creates instance of [SimpleDialog] with [DialogConfig.MacOs] configuration
+     */
     override fun createActionDialog(message: String, action: String): ActionDialog {
-        return ActionDialog(SystemConfig.MacOs, message, action)
+        return ActionDialog(DialogConfig.MacOs, message, action)
     }
 }
 
+/* playground */
 fun main() {
 
     arrayOf(MacOsDialogFactory(), WindowsDialogFactory())
